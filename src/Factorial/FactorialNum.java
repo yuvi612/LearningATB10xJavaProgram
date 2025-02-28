1257c7112e253c69f1efcfea0df43f60c645e654
yuvi612 <yuvaraj612@gmail.com>
2025-02-28 09:03:06 -0800

    ElectricityBillCalc

--- src/Factorial/FactorialNum.java
+++ src/Factorial/FactorialNum.java
@@ -2,6 +2,15 @@ package Factorial;
 
 public class FactorialNum {
     public static void main(String[] args) {
+        int f = 10;
+        int n=1;
+        for(int i = 1; i<=f;i++)
+        {
+
+            n = n *i;
+
+        }
+        System.out.println(n);
 
     }
 }
